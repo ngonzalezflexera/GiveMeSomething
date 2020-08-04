e101ffa99f3511eb2b1bd774c6a54d426ec6a4ea
David González <nodearth@gmail.com>
2020-08-04 09:27:42 +0100

    Now it works

--- client/src/PingComponent.js
+++ client/src/PingComponent.js
@@ -10,7 +10,7 @@ class PingComponent extends Component{
     }
 
     componentWillMount() {
-        axios.get('api/ping')
+        axios.get('http://localhost:8080/api/ping')
             .then((response)=> {
                 this.setState(()=> {
                     return {pong : response.data.message}
--- server/routes/routes.go
+++ server/routes/routes.go
@@ -6,20 +6,17 @@ import (
 	"auth/middleware"
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Handlers(db datastore.Datastore) *mux.Router {
 	r := mux.NewRouter().StrictSlash(true)
 	// Serve static files
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("../client/build/static/"))))
-	r.PathPrefix("/build/").Handler(http.StripPrefix("/build/", http.FileServer(http.Dir("../client/build/static/"))))
-	// Serve index page on all unhandled routes
-	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../client/build/index.html")
-	})
+	r.PathPrefix("/app/").Handler(http.StripPrefix("/app/", http.FileServer(http.Dir("../client/build/"))))
 
+	// Serve index page on all unhandled routes
 	r.HandleFunc("/api/ping", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Print("Pong")
 		writer.WriteHeader(http.StatusOK)
